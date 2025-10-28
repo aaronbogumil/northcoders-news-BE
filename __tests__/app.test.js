b4fc7f3e73e500f20da573ca7513973cc9b151cf
aaronbogumil <aaronbogumil@gmail.com>
2025-10-28 14:09:46 +0000

    work in progress for error handlers

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,88 +1,150 @@
-const db = require("../db/connection")
-const seed = require("../db/seeds/seed")
-const data = require("../db/data/test-data")
-const request = require("supertest")
-const app = require("../app.js")
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+const request = require("supertest");
+const app = require("../app.js");
 
 beforeEach(() => {
-    return seed(data)
-})
+  return seed(data);
+});
 
 afterAll(() => {
-    return db.end()
-})
+  return db.end();
+});
 
-describe("GET /api/topics", () => { 
-    test("200: Responds with the topics object",() => { 
-        return request(app)
-            .get('/api/topics')
-            .expect(200)
-            .then(({body}) => {
-                expect(body.length).toBe(3)
-                expect(Array.isArray(body)).toBe(true)
-                expect(body).toEqual([
-                    {slug: 'mitch', description: 'The man, the Mitch, the legend', img_url: ''},
-                    {slug: 'cats', description: 'Not dogs', img_url: ''},
-                    {slug: 'paper', description: 'what books are made of', img_url: ''}
-                ])
-               
-            })
-    })
-})
+describe("GET /api/topics", () => {
+  test("200: Responds with the topics object", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.length).toBe(3);
+        expect(Array.isArray(body)).toBe(true);
+        expect(body).toEqual([
+          { slug: "mitch", description: "The man, the Mitch, the legend", img_url: "" },
+          { slug: "cats", description: "Not dogs", img_url: "" },
+          { slug: "paper", description: "what books are made of", img_url: "" },
+        ]);
+      });
+  });
+});
 
-describe.only("GET /api/articles", () => { 
-    test('should give an array of 13 articles', () => {
-        return request(app)
-            .get('/api/articles')
-            .expect(200)
-            .then(({body})=>{
-                expect(body.articles.length).toBe(13)
-            })
-        
-    });
-    test("should respond with 200: 'Responds with the articles object 'when passed no comment count but  and all values from the articles table",() => { 
-        return request(app)
-            .get('/api/articles')
-            .expect(200)
-            .then(({body}) => {
-                body.articles.forEach(article => {
-                expect(article).toMatchObject({
-                    author: expect.any(String),
-                    title: expect.any(String),
-                    article_id: expect.any(Number),
-                    topic: expect.any(String),
-                    votes: expect.any(Number),
-                    article_img_url: expect.any(String),
-                })
-                })
-            })
-    })
-    test("should respond with 200: 'Responds with the articles object 'when passed no comment count but  and all values from the articles table excluding the body", () => {
-        return request(app)
-            .get('/api/articles')
-            .expect(200)
-            .then(({body}) => {
-                body.articles.forEach(article => {
-                    article.hasOwnProperty("body") === false
-                })
-            })
-    });
-    test("should respond with 200: 'Responds with the articles object 'when passed a comment count  and all values from the articles table excluding the body", () => {
-        return request(app)
-            .get('/api/articles')
-            .expect(200)
-            .then(({body}) => {
-                body.articles.forEach(article => {
-                    expect(article).toMatchObject({
-                    author: expect.any(String),
-                    title: expect.any(String),
-                    article_id: expect.any(Number),
-                    topic: expect.any(String),
-                    votes: expect.any(Number),
-                    article_img_url: expect.any(String),
-                    comment_count: expect.any(Number),
-                    })
-                })
-            })
-    });
-})
+describe("GET /api/articles", () => {
+  test("should give an array of 13 articles", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles.length).toBe(13);
+      });
+  });
+  test("should respond with 200: 'Responds with the articles object 'when passed no comment count but  and all values from the articles table", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        body.articles.forEach((article) => {
+          expect(article).toMatchObject({
+            author: expect.any(String),
+            title: expect.any(String),
+            article_id: expect.any(Number),
+            topic: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+          });
+        });
+      });
+  });
+  test("should respond with 200: 'Responds with the articles object 'when passed no comment count but  and all values from the articles table excluding the body", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        body.articles.forEach((article) => {
+          article.hasOwnProperty("body") === false;
+        });
+      });
+  });
+  test("should respond with 200: 'Responds with the articles object 'when passed a comment count and all values from the articles table excluding the body", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        body.articles.forEach((article) => {
+          expect(article).toMatchObject({
+            author: expect.any(String),
+            title: expect.any(String),
+            article_id: expect.any(Number),
+            topic: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            comment_count: expect.any(Number),
+          });
+        });
+      });
+  });
+});
+describe("GET /api/users", () => {
+  test("should respond with 200 and the users object", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body }) => {
+        body.users.forEach((user) => {
+          expect(user).toMatchObject({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
+describe("GET /api/articles/:article_id", () => {
+  test("should respond with 200 and article matching the article_id ", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article[0]).toMatchObject({
+          author: "icellusedkars",
+          title: "Eight pug gifs that remind me of mitch",
+          article_id: 3,
+          body: "some gifs",
+          topic: "mitch",
+          created_at: "2020-11-03T09:12:00.000Z",
+          votes: 0,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+   test.skip("should respond with 400 error when given an invalid type for an index ", () => {
+    return request(app)
+      .get("/api/articles/not_an_index")
+      .expect(400)
+      .then(({ body }) => {
+        console.log(body)
+        expect(body.msg).toEqual("Bad request")
+      });
+  });
+});
+describe("GET /api/articles/:article_id/comments", () => {
+  test("should respond with 200 and article matching the article_id ", () => {
+    return request(app)
+      .get("/api/articles/3/comments")
+      .expect(200)
+      .then(({ body }) => {
+        body.comments.forEach((comment) => {
+          expect(comment).toMatchObject({
+            comment_id: expect.any(Number),
+            votes: expect.any(Number),
+            created_at: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            article_id: expect.any(Number),
+          });
+        });
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,34 +1,69 @@
-const express = require("express");
+const express = require("express")
 const app = express();
 const db = require("./db/connection");
 
-
 app.use(express.json());
 
+app.use('/api', express.static('./public'))
+
 app.get("/", (req, res) => {
-    res.send("Connected at endpoint: /")
-})
+  res.send("Connected at endpoint: /");
+});
 app.get("/api/topics", (req, res) => {
-    db.query("SELECT * FROM topics",).then((({rows})=>{
-        res.status(200).send(rows)
-    }))
-    
+  db.query("SELECT * FROM topics").then(({ rows }) => {
+    res.status(200).send(rows);
+  });
 });
 
 app.get("/api/articles", (req, res) => {
-    const articleQuery = `SELECT a.author, a.title, a.article_id, a.topic, a.created_at, a.votes, a.article_img_url, 
+  const articleQuery = `SELECT a.author, a.title, a.article_id, a.topic, a.created_at, a.votes, a.article_img_url, 
     COUNT(c.comment_id)::INT AS comment_count 
     FROM articles a 
     LEFT JOIN comments c ON a.article_id = c.article_id
-    GROUP BY a.article_id`
-    db.query(articleQuery).then(({rows})=>{//(SELECT COUNT(*) AS comment_count FROM comments WHERE a.article_id = c.article_id)
-            res.status(200).send({articles : rows})
-    })
+    GROUP BY a.article_id`;
+  return db.query(articleQuery).then(({ rows }) => {
+    res.status(200).send({ articles: rows });
+  });
+});
+app.get("/api/articles/:article_id", (req, res) => {
+  let { article_id } = req.params;
+  console.log(article_id);
+  const articleQuery = `SELECT author, title, article_id, body, topic, created_at, votes, article_img_url FROM articles WHERE article_id = $1;`;
+  article_id = Number(article_id);
+  if (article_id) {
+    return db.query(articleQuery, [article_id]).then(({ rows }) => {
+      res.status(200).send({ article: rows });
+    });
+  }
+});
+app.get("/api/articles/:article_id/comments", (req, res) => {
+  let { article_id } = req.params;
+  let articleQuery = `SELECT c.comment_id, c.votes, c.created_at, c.author, c.body, c.article_id FROM comments AS c LEFT JOIN articles AS a ON c.article_id = a.article_id WHERE a.article_id = $1;`;
+  article_id = Number(article_id);
+  if (article_id) {
+    return db.query(articleQuery, [article_id]).then(({ rows }) => {
+      res.status(200).send({ comments: rows });
+    });
+  }
+});
+
+app.get("/api/users", (req, res) => {
+  const usersQuery = "SELECT * FROM users";
+  return db.query(usersQuery).then(({ rows }) => {
+    res.status(200).send({ users: rows });
+  });
 });
 
 app.use((error, request, response, next) => {
-    console.error(error)
-    response.status(500).send(error)
-}
-)
-module.exports = app
+  console.error(error);
+  if (error.code === "22p02") {
+    return response.status(400).send({ msg: "Bad request" });
+  }
+  next(error);
+});
+
+app.use((error, request, response, next) => {
+  console.error(error);
+  response.status(500).send(error);
+});
+module.exports = app;
