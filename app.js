602a138bb854f0b43b1a459fbf8c9f294da38b9e
aaronbogumil <aaronbogumil@gmail.com>
2025-11-07 16:07:53 +0000

    working in progress, hosting

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const db = require("./db/connection");
 
 app.use(express.json());
 
-app.use('/api', express.static('./public'))
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
   res.send("Connected at endpoint: /");
--- listen.js
+++ listen.js
@@ -1,6 +1,11 @@
-const app = require("./app");
+// const app = require("./app");
 
-app.listen(8000, () => {
-    console.log("listening on 8000")
-    }
-)
+// app.listen(8000, () => {
+//   console.log("listening on 8000");
+// });
+
+
+const app = require("./app.js");
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
